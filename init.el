4a4d4e7330f4a5e3958939d5e73a947f2a8d68f3
Tom Purl <tpurl@wi-tronix.com>
2018-12-13 16:06:47 -0600

    Small changes to make the file work across platforms.

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  (expand-file-name "emacs-init.org"
 		   user-emacs-directory))
 
-(setq org-agenda-files (quote ("~/Documents/td/gtd/org/")))
+(setq org-agenda-files (list "~/org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -12,7 +12,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (virtualenvwrapper web-server ecukes feature-mode web-mode graphviz-dot-mode groovy-mode ack htmlize json-reformat yaml-mode powerline magit paredit rainbow-delimiters emojify ahk-mode yasnippet smex powershell org-bullets org moe-theme))))
+    (projectile flycheck org-fancy-priorities use-package markdown-mode jedi virtualenvwrapper web-server ecukes feature-mode web-mode graphviz-dot-mode groovy-mode ack htmlize json-reformat yaml-mode powerline magit paredit rainbow-delimiters emojify ahk-mode yasnippet smex powershell org-bullets org moe-theme)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
